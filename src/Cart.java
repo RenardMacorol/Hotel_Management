60430daef28073cf384eb33019c5ccea6930bc0c
RenardMacorol <rmacorol18@gmail.com>
2024-09-28 14:31:48 +0800

    Done

---
+++ src/Cart.java
@@ -0,0 +1,16 @@
+public class Cart implements HotelService {
+	private int numberOfCarts;
+
+	Cart(int numberOfCarts) {
+		this.numberOfCarts = numberOfCarts;
+	}
+
+	public void service() {
+		requestCard(numberOfCarts);
+	}
+
+	public void requestCard(int numberOfCarts) {
+		System.out.println("No of Carts requested: " + numberOfCarts);
+	}
+
+}
---
+++ src/FrontDesk.java
@@ -0,0 +1,17 @@
+public class FrontDesk {
+	Valet valet;
+	HouseKeeping houseKeeping;
+	Cart cart;
+
+	FrontDesk(String plateNumber, int roomNumber, int numberOfCarts) {
+		valet = new Valet(plateNumber);
+		houseKeeping = new HouseKeeping(roomNumber);
+		cart = new Cart(numberOfCarts);
+	}
+
+	public void executeServices() {
+		valet.service();
+		houseKeeping.service();
+		cart.service();
+	}
+}
